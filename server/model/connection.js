260878936ebd4e20cede2d8483398ccfe0aa2ceb
robertelving <vivalaklask@gmail.com>
2014-11-28 09:30:52 +0100

    db tests merged with feature services

--- server/model/connection.js
+++ server/model/connection.js
@@ -1,3 +1,53 @@
-/**
- * Created by Simon on 26/11/14.
- */
+var mongo = require('mongoose');
+var dbUrl = require('../../config.json').database_connection_url;
+
+function connect(onConnected) {
+    mongo.connect(dbUrl, function (err) {
+        if (onConnected) {
+            return onConnected(err);
+        }
+    });
+}
+
+function close(onClose) {
+    mongo.connection.close(function () {
+        if (onClose) {
+            return onClose();
+        }
+    });
+}
+
+(function () {
+    mongo.connection.on('connected', function () { //
+        console.log("db open");
+    });
+
+    mongo.connection.on('error', function (error) {
+        console.log("db error: " + error);
+    });
+
+    mongo.connection.on('disconnected', function () {
+        console.log("db close");
+    });
+
+    process.on('SIGINT', function () {
+        mongo.connection.close(function () {
+            console.log("db close termination");
+            process.exit(0);
+        });
+    });
+
+    process.on('SIGTERM', function () {
+        mongo.connection.close(function () {
+            console.log("db hard");
+            process.exit(0);
+        });
+    });
+
+})();
+
+
+module.exports = {
+    connect: connect,
+    close: close
+};
--- server/model/models.js
+++ server/model/models.js
@@ -1,3 +1,47 @@
-/**
- * Created by Simon on 26/11/14.
- */
+var mongoose = require('mongoose');
+
+
+var documentation = new mongoose.Schema({
+    doc_id: {type: Number, unique: true, required: true},
+    title: String,
+    subtitle: String,
+    author: String,
+    timestamp: {type: Date, default: new Date},
+    abstract: String,
+    body: String,
+    images: [String],
+    tags: [String],
+    comments: [{body: String, date: Date}]
+});
+
+
+var profile = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
+    resume: String,
+    skills: [String],
+    profile_picture: String,
+    github_link: String,
+    collaborations: [String]
+});
+
+var downloads = new mongoose.Schema({
+    download_id: Number,
+    title: String,
+    description: String,
+    repo_link: String,
+    thumbnail: String
+
+});
+
+
+var Documentation = mongoose.model('documentation', documentation);
+var Profile = mongoose.model('profiles', profile);
+var Download = mongoose.model('downloads', downloads);
+
+
+module.exports = {
+    Documentation: Documentation,
+    Profile: Profile,
+    Download: Download
+};
