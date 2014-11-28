3abe42574b95ac3110ae3938584e691d2d856688
Simon Grønborg <groenborg27@gmail.com>
2014-11-28 13:07:57 +0100

    Title: Rich text editor content tested Description: tried to create a rich content editor using execcommand
    Important: this is not a useable commit
    Alert: nothing new added since thomases last commit
    
    Commit: Simon

--- client/SPA/app.js
+++ client/SPA/app.js
@@ -1,22 +1,27 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('CMSApp', [
+var app = angular.module('CMSApp', [
     'ngRoute',
+    'ngSanitize',
     'CMSApp.controllers',
     'CMSApp.directives',
     'CMSApp.services',
     'CMSApp.factories',
     'CMSApp.filters' // Remember to add views when that's relevant
-]).config(['$routeProvider', function($routeProvider){
+]);
+app.config(['$routeProvider', function ($routeProvider) {
 
-    $routeProvider.when('/justTesting',{
+    $routeProvider.when('/justTesting', {
         templateUrl: 'views/justTesting.html',
         controller: 'justTesting'
     }).otherwise({redirectTo: '/justTesting'});
 
-
-
 }]);
 
-
+app.controller('AppController', ['$scope','$sce', function ($scope, $sce) {
+    $scope.content = "<strong>hej</strong>";
+        $scope.dhtml = function () {
+            return $sce.trustAsHtml($scope.content);
+        }
+}]);
