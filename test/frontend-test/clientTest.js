8b07db54b01ee9f249ed2665f59e7dd8d81267ec
ThomasDrengen <thomashj93@gmail.com>
2014-12-03 15:54:43 +0100

    Title: Karma ready for jasmine test
    Description: Set up file clientTest, will hold both describes for controllers.
    
    Commit: Thomas

---
+++ test/frontend-test/clientTest.js
@@ -0,0 +1,31 @@
+describe('FifthElement', function () {
+
+
+    var testParam = "";
+    var documentationMock = [
+        {
+            "doc_id": 0,
+            "title": "",
+            "subtitle": "",
+            "author": "",
+            "abstract": "",
+            "body": "",
+            "comments": [],
+            "tags": [],
+            "images": []
+        }
+    ];
+
+
+    beforeEach(function () {
+        module('CMSApp.controllers')
+    });
+
+    beforeEach(inject(function ($httpBackend, $rootScope, $controller) {
+    }));
+
+    it("should be defined", function () {
+    });
+
+
+});
