053a4be35ab47baf649ca00b7a6989a315c69bee
groenborg <groenborg27@gmail.com>
2014-12-14 11:31:36 +0100

    Title: all files are .min, added new skin
    Description: The files have been changed to min files.
    I also added a new skin to bootstrap.
    
    The dummy login have also been removed
    
    Commit: Simon

--- server/routes/loginRest.js
+++ server/routes/loginRest.js
@@ -9,45 +9,34 @@ router.post('/authenticate', function (req, response) {
     console.log(req.body);
 
     var credentials = JSON.stringify(req.body);
-//    request.post({
-//        headers: {'content-type': 'application/json'},
-//        url: url,
-//        timeout: 10000,
-//        body: credentials
-//    }, function (err, res, body) {
-//        if (err) {
-//            response.statusCode = 401;
-//            response.send(err);
-//            console.log("server send error");
-//        } else {
-//            if (body == undefined) {
-//                response.statusCode = 401;
-//                response.send("No body defined");
-//            } else {
-//                body = JSON.parse(body);
-//                if (body.err) {
-//                    console.log("error in login");
-//                    response.statusCode = 401;
-//                    response.send(body.err);
-//                } else {
-//                    var token = jwt.sign(body, "secret", {expiresInMinutes: 60 * 5});
-//                    response.statusCode = 200;
-//                    response.json({token: token});
-//                }
-//            }
-//        }
-//    });
-
-    var tmp = {
-        username: "temporary",
-        email: "something",
-        password: ""
-    };
-
-    var token = jwt.sign(tmp, "secret", {expiresInMinutes: 60 * 5});
-    response.statusCode = 200;
-    response.json({token: token});
-
+    request.post({
+        headers: {'content-type': 'application/json'},
+        url: url,
+        timeout: 10000,
+        body: credentials
+    }, function (err, res, body) {
+        if (err) {
+            response.statusCode = 401;
+            response.send(err);
+            console.log("server send error");
+        } else {
+            if (body == undefined) {
+                response.statusCode = 401;
+                response.send("No body defined");
+            } else {
+                body = JSON.parse(body);
+                if (body.err) {
+                    console.log("error in login");
+                    response.statusCode = 401;
+                    response.send(body.err);
+                } else {
+                    var token = jwt.sign(body, "secret", {expiresInMinutes: 60 * 5});
+                    response.statusCode = 200;
+                    response.json({token: token});
+                }
+            }
+        }
+    });
 });
 
 module.exports = router;
