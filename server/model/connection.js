45169664ebd58108e67ee8847b2b5df1c48edbcd
Thomas Hedegaard <thomashj93@gmail.com>
2015-08-25 13:20:45 +0800

    testing

--- server/model/connection.js
+++ server/model/connection.js
@@ -12,7 +12,7 @@ function connect(onConnected, enviroment) {
     });
 }
 
-// another testfefe this is from master yes
+// another testfefefe this is from master yes
 
 function close(onClose) {
     mongo.connection.close(function () {
