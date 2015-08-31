0cd3e4b590bde154d223e0b8125136a22391c577
Thomas Hedegaard <thomashj93@gmail.com>
2015-08-31 09:49:20 +0800

    testing

--- scripts/backupDB.js
+++ scripts/backupDB.js
@@ -8,7 +8,7 @@ var documents = "";
 var profiles = "";
 var downloads = "";
 
-// test6rw
+// testing from master
 
 var getDocuments = function (callback) {
     model.Document.find({}, function (err, data) {
